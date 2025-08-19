3031900b42c7539893a61c3dab792b2b6cfccbbf
Rexy-dat <fgreeyg@gmail.com>
2025-08-19 12:35:13 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,11 @@
 
 document.getElementById("sendBtn")?.addEventListener("click", () => {
-  const targetNumber = document.getElementById("targetNumber").value;
+  const target = document.getElementById("target").value;
   const bug = document.getElementById("bug-type").value;
   fetch("/api/crash", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ targetNumber })
+    body: JSON.stringify({ target })
   })
   .then(res => res.json())
   .then(data => alert(data.message))
